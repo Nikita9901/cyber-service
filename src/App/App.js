47d03d8475522f0949173999eca6c8ff1c2f6070
Nikita9901 <nikitichsn2@gmail.com>
2023-01-17 21:50:29 +0300

    feat: relocate to linux

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,20 @@
 import { HeadBlock, ServiceBlock, FooterBlock } from "../components";
 import background from "./images/background.png";
 import backgroundMob from "./images/backgroundMob.png";
+import line from "./images/line.png";
 import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles.app}>
       <HeadBlock />
+      <div className={styles.transition}>
+        {/*<img src={line} alt="" className={styles.line} />*/}
+      </div>
       <ServiceBlock />
       <FooterBlock />
-      <img src={background} alt="" className={styles.background} />
-      <img src={backgroundMob} alt="" className={styles.backgroundMob} />
+      {/*<img src={background} alt="" className={styles.background} />*/}
+      {/*<img src={backgroundMob} alt="" className={styles.backgroundMob} />*/}
     </div>
   );
 }
